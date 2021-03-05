5867a05bb8f4e692d83f0d826053e20ca31e2329
wch18735 <wch18735@naver.com>
2021-03-05 18:11:28 +0900

    simple python

---
+++ SW Expert Academy/D2/간단한 압축 풀기.py
@@ -0,0 +1,11 @@
+def solution():
+    N = int(input())
+    p_num = 0
+    for i in range(N):
+        a = int(input())
+
+
+if __name__=="__main__":
+    T = int(input())
+    for _ in range(T):
+        solution()
