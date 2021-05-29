6a4dd34cbf193e99934b2faf8205e655e4303328
wch18735 <wch18735@naver.com>
2021-05-29 21:43:49 +0900

    sort NlogN

---
+++ Programmers/Python/완주하지 못한 선수.py
@@ -0,0 +1,14 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    answer = ''
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+
+    if answer == '':
+        answer = participant[-1]
+
+    return answer
